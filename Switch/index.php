22ca2ad1d4decc71e0cb57ab57340d424860894b
showrov4g <showrovg00@gmail.com>
2021-05-02 02:40:27 +0600

    swq

--- Switch/index.php
+++ Switch/index.php
@@ -1,4 +1,16 @@
 <?php
+	$x=50;
 	
+	switch($x)
+	{
+		case 90: echo "it is equal to 90";
+		break;
+		case 80: echo "it is equal to 80";
+		break;
+		case 70: echo "it is equal to 70";
+		break;
+		default : "none fo them";
+	}
+	echo "you have come out";
 
 ?>
