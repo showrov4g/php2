140d63f2fe485a46fa008a06ce6ea3925d9e4938
showrov4g <showrovg00@gmail.com>
2021-05-02 01:23:47 +0600

    if

---
+++ If_else/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body style="font-size: 40px;">
+		<?php
+			# any valu other than zero is positive that
+			$a=87;
+			if($a==90)
+			{
+				echo $a;
+			}
+			else if($a==88)
+			{
+				echo "it is equal to 89";
+			}
+			
+			else
+			{
+				echo "they are not equal";
+			}
+			
+		
+		?>
+	
+	
+	
+	</body>
+</html>
