e8b53ae7e167bd1926def8908d3c78de553caf3d
showrov4g <showrovg00@gmail.com>
2021-05-02 02:04:20 +0600

    con]

--- If_else/index.php
+++ If_else/index.php
@@ -5,26 +5,31 @@
 	</head>
 	<body style="font-size: 40px;">
 		<?php
-			# any valu other than zero is positive that
-			$a=87;
-			if($a==90)
-			{
-				echo $a;
-			}
-			else if($a==88)
-			{
-				echo "it is equal to 89";
-			}
-			
-			else
-			{
-				echo "they are not equal";
-			}
-			
+		# fi else statement
+
+		$a=33;
 		
+		
+		if($a==100)
+		{
+			echo "He will get golden";
+		}
+		else if($a==60)
+		{
+			echo "he is get 60%";
+		}
+		else if($a==40)
+		{
+			echo "he fail in on subject";
+		}
+		else
+		{
+			echo "he is faill";
+		}
+
 		?>
-	
-	
-	
+
+
+
 	</body>
 </html>
